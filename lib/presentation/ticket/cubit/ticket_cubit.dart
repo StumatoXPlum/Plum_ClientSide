b528cd2a55964eb175feea838df00231308a089c
Prajwal <prajwal77d@gmail.com>
2025-03-16 12:28:25 +0530

    booking issue fixed

--- lib/presentation/ticket/cubit/ticket_cubit.dart
+++ lib/presentation/ticket/cubit/ticket_cubit.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../model/ticket_model.dart';
 
 abstract class TicketState extends Equatable {
@@ -34,37 +35,44 @@ class TicketError extends TicketState {
 }
 
 class TicketCubit extends Cubit<TicketState> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final SupabaseClient _supabase = Supabase.instance.client;
+
   TicketCubit() : super(TicketInitial()) {
     _listenToBookings();
   }
 
   Future<void> setTicket(TicketModel ticket) async {
-    final user = FirebaseAuth.instance.currentUser;
-    if (user == null) return;
+    final userId = _getUserId();
+    if (userId == null) {
+      emit(TicketError("User not available"));
+      return;
+    }
 
     try {
-      await FirebaseFirestore.instance
+      await _firestore
           .collection('users')
-          .doc(user.uid)
+          .doc(userId)
           .collection('bookings')
           .add(ticket.toFirestore());
 
       print("Booking saved");
     } catch (e) {
-      emit(TicketError("failed to save:$e"));
+      emit(TicketError("Failed to save booking: $e"));
     }
   }
 
   void _listenToBookings() {
-    final user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
-      emit(TicketError("User not availabe"));
+    final userId = _getUserId();
+    if (userId == null) {
+      emit(TicketError("User not available"));
       return;
     }
 
-    final bookingRef = FirebaseFirestore.instance
+    final bookingRef = _firestore
         .collection('users')
-        .doc(user.uid)
+        .doc(userId)
         .collection('bookings');
 
     bookingRef.snapshots().listen((snapshot) {
@@ -73,5 +81,12 @@ class TicketCubit extends Cubit<TicketState> {
 
       emit(TicketLoaded(tickets));
     });
+  }
+
+  String? _getUserId() {
+    if (_firebaseAuth.currentUser != null) {
+      return _firebaseAuth.currentUser?.uid;
+    }
+    return _supabase.auth.currentUser?.id;
   }
 }
